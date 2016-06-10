8891637440e910bc7da4200ac6b16dd34dd2520e
Riggan Hart <rigganhart@Riggans-MacBook.local>
2016-06-10 13:37:40 -0400

    added delet on click of li

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
   chatPage.init();
+  console.log(user);
 })
 
 var chatPage = {
     url: 'http://tiny-tiny.herokuapp.com/collections/damonrigganloganchat',
     chats:[],
     init: function () {
+
       chatPage.styling();
       chatPage.events();
     },
@@ -19,17 +21,16 @@ $('form ').on('submit', function(event){
   event.preventDefault();
   var newMsg = {
     content: $('input[name="chatbox"]').val(),
-
   };
   console.log(newMsg);
-  $('.chatlog ul').append(`<li> ${newMsg.content} </li>`);
+  $('.chatlog ul').append(`<li> <a href="#>"> <h3>${newMsg.content}</h3> </a></li>`);
   $.ajax({
     url: 'http://tiny-tiny.herokuapp.com/collections/damonrigganloganchat',
     method: "POST",
     data: newMsg,
     success: function(data) {
       console.log("ItWorks", data);
-      $('.chatbox ul').append(`<li> ${newMsg.content} </li>`);
+      $('.chatbox ul').append(`<li> <a href="#>"> <h3>${newMsg.content}</h3> </a></li>`);
       chatPage.getChat();
     },
     error: function(err) {
@@ -37,13 +38,32 @@ $('form ').on('submit', function(event){
     }
   })
 })
+//end of POST
+
+//find delete item
+$(document).on('click', 'a',function(event){
+  event.preventDefault();
+    console.log($(this));
+      var msgId = $(this).parent().data('id');
+      console.log(msgId);
+      chatPage.deleteChat(msgId);
+      $(this).remove();
+  })
 
 
 
-
-
-    },
+},
 // end of events
+
+
+
+
+// getUser: function() {
+// var user = prompt("Please enter your username:");
+// console.log(user);
+//
+// },
+
 
 getChat: function () {
   $.ajax({
@@ -53,15 +73,30 @@ getChat: function () {
       console.log("we got something", data);
       $('.chatlog ul').html("");
       data.forEach(function(chats) {
-        $('.chatlog ul').append(`<li data-id=${chats._id}> ${chats.content} </li>`);
+        $('.chatlog ul').append(`<li data-id=${chats._id}> <a href="#">${chats.content}</a> </li>`);
       })
     },
     error: function(err) {
       console.error("WTF",err);
-    }
-  })
-},
+      }
+    })
+  },
+
+deleteChat: function (msgId) {
+  var deleteUrl = chatPage.url + "/" + msgId;
+  $.ajax({
+    url: deleteUrl,
+    method:"DELETE",
+    success: function(data) {
+      console.log("IT IS GONE",data);
+      chatPage.getChat();
+    },
+    error: function(err) {
+      console.error("you blew it", err);
+      }
+    })
+  },
 
 
-
+//end of chatPage
 }
